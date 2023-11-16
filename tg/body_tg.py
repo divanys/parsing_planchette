1caad960388ab994c09c6857d290033227758a11
divan <hlinakcurbag@gmail.com>
2023-11-16 12:53:41 +0300

    ААА аёграм 3.х дал себя знать, но я тоже не просто диван

--- tg/body_tg.py
+++ tg/body_tg.py
@@ -1,23 +1,25 @@
+import asyncio
 import json
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-from aiogram.dispatcher.filters import Text
-from aiogram.dispatcher import FSMContext
-from aiogram.types import Message
-from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
+import logging
+import sys
+
+from aiogram import Router
+
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.enums import ParseMode
+from aiogram.filters.command import Command
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.types import ReplyKeyboardRemove, KeyboardButton
 import Links_tg
+from aiogram.filters import Filter
+from aiogram.types import Message
 
-storage = MemoryStorage()
-chatbot = Bot(token=Links_tg.api_tg)
-dp = Dispatcher(chatbot, storage=storage)
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
 
-with open('../files_data.json', 'r') as file:
-    dates_data = json.load(file)
-
-with open('../data_concretn.json', 'r') as file:
-    data_concretn = json.load(file)
+router = Router(name=__name__)
 
 
 class DateState(StatesGroup):
@@ -28,90 +30,134 @@ class DateState(StatesGroup):
     waiting_for_concrete = State()
 
 
-@dp.message_handler(commands=['start'])
-async def start_cmd(message: Message):
-    markup = ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.add(KeyboardButton('Дата'))
+# Хэндлер на команду /start
+@router.message(Command("start"))
+async def cmd_start(message: types.Message):
+    kb = [
+        [KeyboardButton(text="Дата")]
+    ]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb)
+
     await message.answer("Привет. Я помогу посмотреть планшетку РКСИ.\n"
-                         "Для навигации используй кнопки ниже.", reply_markup=markup)
+                         "Для навигации используй кнопки ниже.", reply_markup=keyboard)
 
 
-@dp.message_handler(Text(equals='Дата'))
-async def date_handler(message: Message):
-    markup = ReplyKeyboardMarkup(resize_keyboard=True)
-    for date_str in dates_data.keys():
-        markup.add(KeyboardButton(date_str))
+@router.message(F.text.lower() == "дата")
+async def date_command(message: types.Message, state: FSMContext) -> None:
+    with open('../files_data.json', 'r') as f:
+        data = json.load(f)
 
-    await message.answer("Выберите дату:", reply_markup=markup)
-    await DateState.waiting_for_date.set()
+    keyboard = []
+
+    for key in data.keys():
+        button = [KeyboardButton(text=key)]
+        keyboard.append(button)
+
+    keyboard = types.ReplyKeyboardMarkup(keyboard=keyboard)
+
+    await message.answer("Выберите дату:", reply_markup=keyboard)
+    await state.set_state(DateState.waiting_for_date)
 
 
-@dp.message_handler(state=DateState.waiting_for_date)
-async def handle_date_choice(message: Message, state: FSMContext):
+@router.message(DateState.waiting_for_date)
+async def handle_date_choice(message: types.Message, state: FSMContext):
     selected_date = message.text
+
+    with open('../files_data.json', 'r') as f:
+        dates_data = json.load(f)
+
     if selected_date in dates_data:
         await state.update_data(selected_date=selected_date)
-        markup = ReplyKeyboardMarkup(resize_keyboard=True)
-        markup.add(KeyboardButton('Кабинет'), KeyboardButton('Группа'), KeyboardButton('Преподаватель'))
-        await message.answer("Выберите тип данных для поиска:", reply_markup=markup)
-        await DateState.waiting_for_data_type.set()
+        keyboard = types.ReplyKeyboardMarkup(keyboard=[
+            [KeyboardButton(text="Кабинет"), KeyboardButton(text='Группа'),
+             KeyboardButton(text='Преподаватель')]
+        ])
+        await message.answer("Выберите тип данных для поиска:", reply_markup=keyboard)
+        await state.set_state(DateState.waiting_for_data_type)
     else:
         await message.answer("Неверная дата. Выберите дату из списка.")
 
 
-@dp.message_handler(state=DateState.waiting_for_data_type)
-async def handle_data_type_choice(message: Message, state: FSMContext):
+@router.message(DateState.waiting_for_data_type)
+async def handle_data_type_choice(message: types.Message, state: FSMContext):
     data_type = message.text
     await state.update_data(data_type=data_type)
-    await message.answer(f"Введите {data_type.lower()}:")
-    await DateState.waiting_for_value.set()
+    await message.answer(f"Введите {data_type.lower()}:", reply_markup=ReplyKeyboardRemove())
+    await state.set_state(DateState.waiting_for_value)
 
 
-@dp.message_handler(state=DateState.waiting_for_value)
-async def handle_value_input(message: Message, state: FSMContext):
+@router.message(DateState.waiting_for_value)
+async def handle_value_input(message: types.Message, state: FSMContext):
     value = message.text
     await state.update_data(value=value)
-    markup = ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.add(KeyboardButton('Все пары'), KeyboardButton('Конкретная'))
-    await message.answer("Выберите 'Все пары' или 'Конкретная':", reply_markup=markup)
-    await DateState.waiting_for_action.set()
+    keyboard = types.ReplyKeyboardMarkup(keyboard=[
+        [KeyboardButton(text='Все пары'), KeyboardButton(text='Конкретная')]
+    ])
+    await message.answer("Выберите 'Все пары' или 'Конкретная':", reply_markup=keyboard)
+    await state.set_state(DateState.waiting_for_action)
 
 
-@dp.message_handler(state=DateState.waiting_for_action, text='Все пары')
-async def handle_all_classes_choice(message: Message, state: FSMContext):
+class MyFilter(Filter):
+    def __init__(self, my_text: str) -> None:
+        self.my_text = my_text
+
+    async def __call__(self, message: Message) -> bool:
+        return message.text == self.my_text
+
+
+@router.message(DateState.waiting_for_action, F.text.lower() == 'все пары')
+async def handle_all_classes_choice(message: types.Message, state: FSMContext):
     data = await state.get_data()
-    await message.answer(f"Вы выбрали {data['data_type']} {data['value']} за {data['selected_date']} и все пары.")
-    await state.finish()
+    await message.answer(f"Вы выбрали {data['data_type']} {data['value']} за {data['selected_date']} и все пары.",
+                         reply_markup=ReplyKeyboardRemove())
+    await state.clear()
 
-@dp.message_handler(state=DateState.waiting_for_action, text='Конкретная')
-async def handle_concrete_choice(message: Message, state: FSMContext):
+
+@router.message(DateState.waiting_for_action, F.text.lower() == 'конкретная')
+async def handle_concrete_choice(message: types.Message, state: FSMContext):
     data = await state.get_data()
-    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    with open('../data_concretn.json', 'r') as f:
+        data_concretn = json.load(f)
+
+    keyboard = []
+
     for item in data_concretn.get(data['selected_date'], []):
-        markup.add(KeyboardButton(item))
+        button = [KeyboardButton(text=item)]
+        keyboard.append(button)
 
-    await message.answer("Выберите конкретную пару:", reply_markup=markup)
-    await DateState.waiting_for_concrete.set()
+    keyboard = types.ReplyKeyboardMarkup(keyboard=keyboard)
 
-@dp.message_handler(state=DateState.waiting_for_concrete)
-async def handle_concrete_choice(message: Message, state: FSMContext):
-    concrete_value = message.text
+    await message.answer("Выберите конкретную пару:", reply_markup=keyboard)
+    await state.set_state(DateState.waiting_for_concrete)
+
+
+@router.message(DateState.waiting_for_concrete)
+async def handle_concrete_choice_is(message: types.Message, state: FSMContext):
+    num_para = message.text
+    await state.update_data(num_para=num_para)
     data = await state.get_data()
-    await message.answer(f"Вы выбрали конкретную пару {concrete_value} за {data['selected_date']}.")
-    await state.finish()
-
-    # Сбрасываем состояние и предлагаем выбрать новую дату
-    markup = ReplyKeyboardMarkup(resize_keyboard=True)
-    for date_str in dates_data.keys():
-        markup.add(KeyboardButton(date_str))
-
-    await message.answer("Выберите дату:", reply_markup=markup)
+    await message.answer(
+        f"Вы выбрали {data['data_type']} {data['value']} за {data['selected_date']} и {data['num_para']}.",
+        reply_markup=ReplyKeyboardRemove())
+    await state.clear()
 
 
-@dp.message_handler(commands=['help'])
-async def help_cmd(message: Message):
-    await message.answer("Напиши на +79895099849")
+@router.message(DateState.waiting_for_action, Command("help"))
+async def help_cmd(message: types.Message):
+    await message.answer("Напиши на +79895099849", reply_markup=ReplyKeyboardRemove())
+
+
+async def main() -> None:
+    # Initialize Bot instance with a default parse mode which will be passed to all API calls
+    bot = Bot(token=Links_tg.api_tg, parse_mode=ParseMode.HTML)
+    # And the run events dispatching
+    dp = Dispatcher()
+    # ... and all other routers should be attached to Dispatcher
+    dp.include_router(router)
+
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    executor.start_polling(dp)
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
